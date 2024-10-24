6bd029dcc76b834e0b5bbadd2823bf10c1d2e582
ivanlie <ivan.lie@meditap.id>
2024-10-24 15:18:40 +0700

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import prettier from 'prettier'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -20,7 +19,6 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier: prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
