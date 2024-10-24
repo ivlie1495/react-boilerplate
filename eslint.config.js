b7e093d92c52a2d16bb645c1be1cb0229d8aa52c
ivanlie <ivan.lie@meditap.id>
2024-10-24 15:25:28 +0700

    ts and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,16 +5,13 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+      globals: globals.browser,
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -22,10 +19,6 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'off',
-        { allowConstantExport: true },
-      ],
     },
   },
 )
