9d2f6ad590b7b031d7f1c858eba679be71ff3c78
ivanlie <ivan.lie@meditap.id>
2024-11-11 18:10:18 +0700

    eslint config prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -11,8 +11,7 @@ export default tseslint.config(
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      ...reactHooks.configs.recommended,
-      ...eslintPluginPrettier,
+      eslintPluginPrettier,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -25,6 +24,9 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
     },
   },
 )
