4533c844fb678bd32597dbf8def7365f95e31d25
ivanlie <ivan.lie@meditap.id>
2024-11-27 09:09:43 +0700

    type safe router

--- src/app.tsx
+++ src/app.tsx
@@ -10,6 +10,13 @@ const router = createRouter({
   routeTree,
 })
 
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 function App() {
   const { isLoaded, user } = useUser()
 
