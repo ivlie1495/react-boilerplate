2dd03fb7bd186a5d7b5b1b7c99d29d7ffe236c0f
ivanlie <ivan.lie@meditap.id>
2025-02-25 00:26:35 +0700

    env api url

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
